34dc9cffc70de2b1b633f70c9c394b2cf3e0dfab
James <jfining@gmail.com>
2015-10-11 17:30:51 -0400

    Finally got the correct output, also added support for more cases.

--- hw3.cpp
+++ hw3.cpp
@@ -5,14 +5,12 @@
 using namespace std;
 
 void binarySearch(ifstream& input){
-	//parse the necessary information from the txt, first size of list
+	//parse the necessary information from the txt, first being the size of list
 	string size;
 	getline(input,size);
-	if (size.find("Cases") != string::npos){
-		getline(input,size);
-	}
 	int intSize = atoi(size.c_str());
-	cout<<"Length of list: "<<intSize<<endl;
+	//cout<<"Length of list: "<<intSize<<endl;
+	
 	//second, the list itself
 	string list;
 	int myList[intSize];
@@ -33,53 +31,37 @@ void binarySearch(ifstream& input){
 			count++;
 		}
 	}
-	
+	/*
 	cout<<"List: ";
 	for(int i=0; i<intSize; i++){
 		cout<<myList[i]<<", ";
 	}
+	*/
 	//and third, what we're searching for
 	string temp;
 	getline(input, temp);
 	int target = atoi(temp.c_str());
-	cout<<endl<<"Target: "<<target<<endl;
-	
-	//now we actually do the search
-	//handle case for target outside the bounds
-	//(preferred but doesn't produce "NOT FOUND 5" for the second trial)
-	/*
-	if(target > myList[intSize-1] || target < myList[0]){
-		cout<<"Target is outside bounds of the list!"<<endl;
-		return;
-	}
-	*/
+	//cout<<endl<<"Target: "<<target<<endl;
 	
 	//search within list:
 	int i=0;
 	int j=intSize;
 	int guess = 0;
-	int iterations = 1;
-	bool success = 0;
-	while (i<j){
-		//infinite loop when i=1 and j=2 and neither are the target
-		//i would get set to 1 each time when list[1]<target
-		if(i==j-1 && myList[i]!=target && myList[j]!=target){
-			break;
-		}
-		//cout<<"i= "<<i<<" j= "<<j<<endl;
-		//cout<<"iterations: "<<iterations<<endl;
+	int iterations = 0;
+	bool success = false;
+	while (i<=j){
 		guess=(i+j)/2;
-		//cout<<"first if"<<endl;
 		if(myList[guess] > target){
-			j=guess;
+			j=guess-1;
 			iterations++;
 		}
 		else if(myList[guess] < target){
-			i=guess;
+			i=guess+1;
 			iterations++;
 		}
 		else{
 			//found it!
+			iterations++;
 			cout<<myList[guess]<<" "<<iterations<<endl;
 			success=true;
 			break;
@@ -93,13 +75,15 @@ void binarySearch(ifstream& input){
 
 int main() {
 	ifstream input;
-	string output = "";
 	input.open("hw3.txt");
-	binarySearch(input);
-	binarySearch(input); //investigate NOT FOUND 6 instead of 5
-	binarySearch(input);
+	string cases;
+	//assuming the cases line will always be in the format: Cases: #
+	getline(input, cases, ' ');
+	getline(input, cases);
+	int intCases = atoi(cases.c_str());
+	for (int i=0; i<intCases; i++){
+		binarySearch(input);
+	}
 	input.close();
-
-	
 return 0;	
 }
